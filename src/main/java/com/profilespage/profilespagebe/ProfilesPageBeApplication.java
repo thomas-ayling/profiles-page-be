ad5fd84cd25aefb41fe16aa494d9f4652b8e0dde
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-02-15 14:02:43 +0000

    Initial Commit

---
+++ src/main/java/com/profilespage/profilespagebe/ProfilesPageBeApplication.java
@@ -0,0 +1,13 @@
+package com.profilespage.profilespagebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesPageBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilesPageBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/profilespage/profilespagebe/ProfilesPageBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profilespage.profilespagebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilesPageBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
